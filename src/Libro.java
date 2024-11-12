874a8b78741f9c64219d61ea6ba6abbae826e53f
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-11-11 19:09:24 -0500

    Add excercise 2

---
+++ src/Libro.java
@@ -0,0 +1,43 @@
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int numPaginas;
+
+    public Libro(String titulo, String autor, int numPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+    }
+
+    public void imprimirResumen() {
+        System.out.println("Título: " + titulo + ", Autor: " + autor);
+    }
+
+    public boolean tieneMasDe300Paginas() {
+        return numPaginas > 300;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+}
